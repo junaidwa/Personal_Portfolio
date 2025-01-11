b00b4744e3eb15cd308a7221d50a74af2057aa5a
Muhammad Junaid <junaidwattoo33@gmail.com>
2025-01-11 06:46:25 +0500

    Controls JavaScript section completed

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var OpenBtn = document.querySelector(".open");
 var Closebtn = document.querySelector(".close");
 var MainBody = document.querySelector(".main");
 
+let counters = document.querySelectorAll(".controls span");
+        let container = document.querySelector(".controls");
+
 // Function to close the navigation
 function closeNavigation() {
     CloseNav.classList.add("hide");
@@ -35,3 +38,50 @@ MainBody.addEventListener('click', function(event) {
 CloseNav.addEventListener('click', function(event) {
     event.stopPropagation(); // Prevent click from propagating to MainBody
 });
+
+
+
+//SUN Moon toggle functionaliy in navbar
+// Select the div and icons
+const themeToggle = document.getElementById('themeToggle');
+const sunIcon = document.getElementById('sunIcon');
+const moonIcon = document.getElementById('moonIcon');
+
+// Add event listener to the div
+themeToggle.addEventListener('click', function() {
+  // Toggle visibility of icons
+  if (sunIcon.style.display === 'none') {
+    sunIcon.style.display = 'block';
+    moonIcon.style.display = 'none';
+  } else {
+    sunIcon.style.display = 'none';
+    moonIcon.style.display = 'block';
+  }
+});
+
+
+//Counter JavaScript
+
+        window.addEventListener("scroll", () => {
+            let containerTop = container.offsetTop;
+            let containerHeight = container.offsetHeight;
+            let scrollPosition = window.pageYOffset;
+
+            if (scrollPosition > containerTop - window.innerHeight + 200 || scrollPosition < containerTop - window.innerHeight + 200) {
+                counters.forEach(counter => {
+                    counter.innerText = 0;
+                    let count = 0;
+                    function updateCount() {
+                        const target = parseInt(counter.dataset.count);
+                        if (count < target) {
+                            count++;
+                            counter.innerText = count;
+                            setTimeout(updateCount, 10);
+                        } else {
+                            counter.innerText = target;
+                        }
+                    }
+                    updateCount();
+                });
+            }
+        });
